4814edd57a2eafd2398ddd1ba42396bc661a39ca
Josue Jimenez <juanjosuejimeenzcervantes@gmail.com>
2026-05-04 20:18:12 -0600

    Implementacion de seccion

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Navbar from "./components/navbar.jsx";
-
+import Hero from "./components/hero.jsx";
 const App = ()  =>{
     return (
         <main>
             <Navbar/>
+            <Hero/>
         </main>
     )
 }
---
+++ src/components/hero.jsx
@@ -0,0 +1,20 @@
+import {useEffect, useRef} from "react";
+
+const Hero = () => {
+    const videoRef = useRef();
+    useEffect(() => {
+        if (videoRef.current) videoRef.current.playbackRate=2;
+    }, []);
+    return (
+        <section id="hero">
+            <div>
+                <h1>MackBock Pro</h1>
+                <img src="/title.png" alt="Mackbock Title"/>
+            </div>
+            <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline />
+            <button>buy</button>
+            <p>from $1599 or $133/ mo for 12 months</p>
+        </section>
+    )
+}
+export default Hero;
